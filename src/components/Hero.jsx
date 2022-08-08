304eaba2b715e51f21c88d33a87fcad7adba8276
Grace Adelaka <adelakagrace@gmail.com>
2022-08-08 16:57:04 +0100

    added text-part

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,7 +1,20 @@
 import React from "react";
+import "../styles/Hero.css";
 
 const Hero = () => {
-  return <section>Hero Section</section>;
+  return (
+    <main className="hero">
+      <div className="row">
+        <div className="col text-part">
+          <h1>
+            We are <span>Picmeup</span> it’s all about delivery parcels on time
+            anywhere
+          </h1>
+        </div>
+        <div className="col img-part"></div>
+      </div>
+    </main>
+  );
 };
 
 export default Hero;
