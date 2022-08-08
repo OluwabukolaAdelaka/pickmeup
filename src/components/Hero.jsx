9b40b8eb1dd4ac712a6c39032895c2c70a858d01
Grace Adelaka <adelakagrace@gmail.com>
2022-08-08 17:55:12 +0100

    hero section done

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,17 +1,26 @@
 import React from "react";
+import HeroImg from "../assets/pana.png";
 import "../styles/Hero.css";
 
 const Hero = () => {
   return (
-    <main className="hero">
+    <main className="hero-section">
       <div className="row">
-        <div className="col text-part">
-          <h1>
-            We are <span>Picmeup</span> it’s all about delivery parcels on time
-            anywhere
+        <div className="col col-md-5">
+          <h1 className="hero-msg">
+            We are <span>Picmeup</span>
           </h1>
+          <h1 className="hero-msg">
+            it’s all about delivery parcels on time anywhere
+          </h1>
+          <p className="hero-text">
+            We make sending and receiving percel accros the globe easy.
+          </p>
+          <button className="hero-btn">Sign UP</button>
         </div>
-        <div className="col img-part"></div>
+        <div className="col col-md-7">
+          <img src={HeroImg} className="img-fluid" alt="hero-img" />
+        </div>
       </div>
     </main>
   );
