06dcbf0d7cb3d9200cb6cb99f2497988a2620ac1
Grace Adelaka <adelakagrace@gmail.com>
2022-08-08 22:38:38 +0100

    services component done

---
+++ src/components/Services.jsx
@@ -0,0 +1,61 @@
+import React from "react";
+import "../styles/Services.css";
+
+const Services = () => {
+  return (
+    <section className="service-section">
+      <h1 className="service-hero-title">Our Services</h1>
+      <div className="row">
+        <div className="col service">
+          <h3 className="service-title">International Delivery</h3>
+          <p className="service-msg">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Soluta,
+            modi iste. Commodi voluptas assumenda incidunt earum deserunt
+            accusamus ipsum itaque ipsa? Ex deleniti molestiae, assumenda
+            nostrum vitae nam, quibusdam iusto facere porro facilis error.
+            Aliquam perspiciatis, possimus soluta voluptate assumenda quod
+            repellendus maxime voluptatem obcaecati quo quae alias ea minus
+          </p>
+        </div>
+
+        <div className="col service">
+          <h3 className="service-title">Cargo Service</h3>
+          <p className="service-msg">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Soluta,
+            modi iste. Commodi voluptas assumenda incidunt earum deserunt
+            accusamus ipsum itaque ipsa? Ex deleniti molestiae, assumenda
+            nostrum vitae nam, quibusdam iusto facere porro facilis error.
+            Aliquam perspiciatis, possimus soluta voluptate assumenda quod
+            repellendus maxime voluptatem obcaecati quo quae alias ea minus
+          </p>
+        </div>
+      </div>
+      <div className="row">
+        <div className="col service">
+          <h3 className="service-title">Import</h3>
+          <p className="service-msg">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Soluta,
+            modi iste. Commodi voluptas assumenda incidunt earum deserunt
+            accusamus ipsum itaque ipsa? Ex deleniti molestiae, assumenda
+            nostrum vitae nam, quibusdam iusto facere porro facilis error.
+            Aliquam perspiciatis, possimus soluta voluptate assumenda quod
+            repellendus maxime voluptatem obcaecati quo quae alias ea minus
+          </p>
+        </div>
+        <div className="col service">
+          <h3 className="service-title">Consultancy</h3>
+          <p className="service-msg">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Soluta,
+            modi iste. Commodi voluptas assumenda incidunt earum deserunt
+            accusamus ipsum itaque ipsa? Ex deleniti molestiae, assumenda
+            nostrum vitae nam, quibusdam iusto facere porro facilis error.
+            Aliquam perspiciatis, possimus soluta voluptate assumenda quod
+            repellendus maxime voluptatem obcaecati quo quae alias ea minus
+          </p>
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default Services;
