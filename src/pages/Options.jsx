313f47ca9672a89ad7f5eda329c41bc030d72fe0
Grace Adelaka <adelakagrace@gmail.com>
2022-08-18 21:41:37 +0100

    options for mobile added

--- src/pages/Options.jsx
+++ src/pages/Options.jsx
@@ -14,14 +14,124 @@ const Options = () => {
       <div className="row pt-5 d-flex justify-content-center align-items-center px-1">
         <div className="d-none col"></div>
         <div className="col-sm-12 col-md-8">
-          <div className="card border-light d-flex justify-content-center align-items-center">
+          <div className="card border-light d-flex justify-content-center align-items-center relative">
             <img
               src={Circle}
-              className="d-none d-md-block card-img img-fluid"
+              className="d-none d-md-block card-img img-fluid relative"
               style={{ height: "55rem", width: "auto" }}
               alt="..."
             />
-            <div className="card-img-overlay pt-5">
+
+            {/* mobile options-card */}
+            <div className="d-md-none card border-light pt-5 absolute">
+              <h3 className="text-center mt-4  mycard-hero-title">Why Us?</h3>
+              <div className="row row-cols-1 row-cols-md-3 g-4 mt-5">
+                <div className="col text-center">
+                  <div
+                    className="card p-4 border-light"
+                    style={{ backgroundColor: "#efefef" }}
+                  >
+                    <div className="card-body">
+                      <img
+                        src={CreditCardImg}
+                        className="img-fluid"
+                        alt="..."
+                      />
+                      <h5 className="card-title mycard-title">
+                        Seemless payment option
+                      </h5>
+                      <p className="card-text mycard-msg">
+                        At pickmeup service we provide you with different
+                        payment options for paying for our services.
+                      </p>
+                    </div>
+                  </div>
+                </div>
+                <div className="col text-center">
+                  <div
+                    className="card p-4 border-light"
+                    style={{ backgroundColor: "rgba(251, 237, 49, 0.4)" }}
+                  >
+                    <div className="card-body">
+                      <img src={AwardImg} className="img-fluid" alt="..." />
+                      <h5 className="card-title mycard-title">
+                        International Partners
+                      </h5>
+                      <p className="card-text mycard-msg">
+                        We parner with international logistics company for our
+                        express delivery aand cargo delivery to avoid
+                        dissapointment.
+                      </p>
+                    </div>
+                  </div>
+                </div>
+                <div className="col text-center">
+                  <div
+                    className="card p-4 border-light"
+                    style={{ backgroundColor: "#e5e5e5" }}
+                  >
+                    <div className="card-body">
+                      <img src={GiftImg} className="img-fluid" alt="..." />
+                      <h5 className="card-title mycard-title">
+                        Timely Delivery
+                      </h5>
+                      <p className="card-text mycard-msg pb-5">
+                        Delivery has never been this fast and esay.
+                      </p>
+                    </div>
+                  </div>
+                </div>
+                <div className="col text-center">
+                  <div
+                    className="card p-4 border-light"
+                    style={{ backgroundColor: "rgba(251, 237, 49, 0.4)" }}
+                  >
+                    <div className="card-body">
+                      <img src={SlackImg} className="img-fluid" alt="..." />
+                      <h5 className="card-title mycard-title">Insurance</h5>
+                      <p className="card-text mycard-msg">
+                        Goods and parcel are insured according to international
+                        standard .
+                      </p>
+                    </div>
+                  </div>
+                </div>
+                <div className="col text-center">
+                  <div
+                    className="card p-4 border-light"
+                    style={{ backgroundColor: "#d9d9d9" }}
+                  >
+                    <div className="card-body">
+                      <img src={TruckImg} className="img-fluid" alt="..." />
+                      <h5 className="card-title mycard-title">Free Pickup</h5>
+                      <p className="card-text mycard-msg">
+                        Free pickup for all shipment. We are trustworty ,
+                        reliable and professional.
+                      </p>
+                    </div>
+                  </div>
+                </div>
+                <div className="col text-center">
+                  <div
+                    className="card p-4 border-light"
+                    style={{ backgroundColor: "rgba(251, 237, 49, 0.4)" }}
+                  >
+                    <div className="card-body">
+                      <img src={MapImg} className="img-fluid" alt="..." />
+                      <h5 className="card-title mycard-title">
+                        Track Shipment
+                      </h5>
+                      <p className="card-text mycard-msg pb-4">
+                        We offer free pickup for all shipment.
+                      </p>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+
+            {/* desktop options card */}
+            <div className="d-none d-md-block card-img-overlay pt-5 absolute">
               <h3 className="text-center mt-4  mycard-hero-title">Why Us?</h3>
               <div className="row row-cols-1 row-cols-md-3 g-4 mt-5">
                 <div className="col text-center">
