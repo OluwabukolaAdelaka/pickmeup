f50e97a88f66d8dc980f3f81e3579e58a69e779d
Grace Adelaka <adelakagrace@gmail.com>
2022-08-19 08:27:18 +0100

    navbar mobile dropdown toggle functioning

--- src/components/Navigation.jsx
+++ src/components/Navigation.jsx
@@ -12,14 +12,14 @@ const Navigation = () => {
           className="navbar-toggler"
           type="button"
           data-bs-toggle="collapse"
-          data-bs-target="#navbarNavAltMarkup"
-          aria-controls="navbarNavAltMarkup"
+          data-bs-target="#navbarSupportedContent"
+          aria-controls="navbarSupportedContent"
           aria-expanded="false"
           aria-label="Toggle navigation"
         >
           <span className="navbar-toggler-icon"></span>
         </button>
-        <div className="collapse navbar-collapse " id="navbarNav">
+        <div className="collapse navbar-collapse" id="navbarSupportedContent">
           <ul className="navbar-nav ms-auto">
             <li className="nav-item">
               <Link className="nav-link pmu-navlinks" to="/">
